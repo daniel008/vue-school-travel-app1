026f70639f3c25fe1f3a55751dafd3db01f45f87
DanielMa <mwldaniel_008@126.com>
2022-03-21 18:53:20 +0800

    Dynamic Routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import Home from '@/view/Home.vue'
+import Home from '@/views/Home.vue'
 
 const routes = [
   {path: '/', name: 'Home', component: Home},
-  {path: '/brazil', name: 'Brazil', component: () => import('@/view/Brazil.vue')},
-  {path: '/hawaii', name: 'Hawaii', component: () => import('@/view/Hawaii.vue')},
-  {path: '/jamaica', name: 'Jamaica', component: () => import('@/view/Jamaica.vue')},
-  {path: '/panama', name: 'Panama', component: () => import('@/view/Panama.vue')}
+  {path: '/brazil', name: 'Brazil', component: () => import('@/views/Brazil.vue')},
+  {path: '/hawaii', name: 'Hawaii', component: () => import('@/views/Hawaii.vue')},
+  {path: '/jamaica', name: 'Jamaica', component: () => import('@/views/Jamaica.vue')},
+  {path: '/panama', name: 'Panama', component: () => import('@/views/Panama.vue')},
+  {path: '/destination/:id', component: () => import('@/views/DestionationShow.vue')}
 ]
 
 const router = createRouter({
