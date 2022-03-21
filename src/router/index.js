18bf6c65a510d642f84e237f09af696cdab6014f
DanielMa <mwldaniel_008@126.com>
2022-03-22 07:02:26 +0800

    Add Experience Card

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,13 @@ const routes = [
     path: '/destination/:id/:slug', 
     name: 'destination.show', 
     component: () => import('@/views/DestionationShow.vue'),
-    props: route => ({id: parseInt(route.params.id)})
+    props: route => ({...route.params, id: parseInt(route.params.id)})
+  },
+  {
+    path: '/destination/:id/:slug/:experienceSlug',
+    name: 'experience.show', 
+    component: () => import('@/views/ExperienceShow.vue'),
+    props: route => ({...route.params, id: parseInt(route.params.id)})
   }
 ]
 
--- src/views/DestionationShow.vue
+++ src/views/DestionationShow.vue
@@ -1,17 +1,34 @@
 <template>
   <section v-if="destination" class="destination">
     <h1>{{destination.name}}</h1>
-    <div class="destination-detaila">
+    <div class="destination-details">
       <img :src="`/images/${destination.image}`" :alt="destination.name">
       <p>{{destination.description}}</p>
+    </div>
+  </section>
+
+  <section class="experiences">
+    <h2>Top Experiences in {{destination.name}}</h2>
+    <div class="cards">
+      <router-link
+          v-for="experience in destination.experiences" 
+          :key="experience.slug"
+          :to="{name: 'experience.show', params:{experienceSlug: experience.slug}}"
+      >
+        <ExperienceCards
+          :experience="experience"
+        />
+      </router-link>
     </div>
   </section>
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import ExperienceCards from '@/components/ExperienceCards.vue'
 
 export default {
+  components: {ExperienceCards},
   props: {
     id: {type: Number, required: true}
   },
