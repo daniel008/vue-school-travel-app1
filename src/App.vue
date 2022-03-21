203fd9948c0477c655e1629fffc66b3e8da39fd6
DanielMa <mwldaniel_008@126.com>
2022-03-21 13:31:08 +0800

    Add Navigation Component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/panama">Panama</router-link>
-  </div>
+  <TheNavigation/>
+
   <div class="container">
     <router-view></router-view>
   </div>
@@ -13,6 +8,10 @@
 
 
 <script>
+ import TheNavigation from "./components/TheNavigation.vue";
 
+ export default {
+   components: {TheNavigation}
+ }
 </script>
 
