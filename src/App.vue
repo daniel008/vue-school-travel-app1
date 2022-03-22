05a50912b3b53c3cee5248ea3884dcbdd385a2da
DanielMa <mwldaniel_008@126.com>
2022-03-22 16:14:37 +0800

    Route Transitions

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <TheNavigation/>
 
   <div class="container">
-    <router-view :key="$route.path"></router-view>
+    <router-view v-slot="{Component}">
+      <transition name="fade" mode="out-in"> 
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -15,3 +19,41 @@
  }
 </script>
 
+<style lang="css">
+  /*out-in mode for slide classes*/
+  /* .slide-enter-active,
+  .slide-leave-active {
+    transition: opacity 1s, transform 1s;
+  }
+  .slide-enter-from,
+  .slide-leave-to {
+    opacity: 0;
+    transform: translateX(-30%)
+  } */
+  /* moveUp classes */
+  /* .moveUp-enter-active{
+    animation: fadeIn 1s ease-in;
+  }
+  @keyframes fadeIn {
+    0% { opacity: 0; }
+    50% { opacity: 0.5; }
+    100% { opacity: 1; }
+  }
+  .moveUp-leave-active {
+    animation: moveUp 0.3s ease-in;
+  }
+  @keyframes moveUp {
+    0% { transform: translateY(0);}
+    100% { transform: translateY(-400px);}
+  } */
+
+  /* out-in mode for fade classes */
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.3s;
+  }
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
