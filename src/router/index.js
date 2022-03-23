8f7686d93d0bdcf7c80a3c75fab4ed18e0f45523
DanielMa <mwldaniel_008@126.com>
2022-03-23 09:53:44 +0800

    Add 404 NotFound Page

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ const routes = [
 
     ]
   },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: () => import('@/views/NotFound.vue')
+  }
 ]
 
 const router = createRouter({
