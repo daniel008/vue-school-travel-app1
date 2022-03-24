40ebcd1c01c32ca55145ecf988b3d1df74d6e483
DanielMa <mwldaniel_008@126.com>
2022-03-24 16:20:46 +0800

    Router and the Composition API

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,20 +12,21 @@
 </template>
 
 <script>
+import {ref} from 'vue'
+import {useRouter, useRoute} from 'vue-router'
 export default {
-  data() {
-    return {
-      username: '',
-      password: ''
-    }
-  },
-  methods: {
-    login() {
+  setup(){
+    const username = ref('')
+    const password = ref('')
+    const route = useRoute()
+    const router = useRouter()
+    const login = () => {
       // Auth user against API
-      window.user = this.username
-      const redirectPath = this.$route.query.redirect || 'protected'
-      this.$router.push(redirectPath)
+      window.user = username.value
+      const redirectPath = route.query.redirect || 'protected'
+      router.push(redirectPath)
     }
+      return {username, password, login}
   }
 }
 </script>
